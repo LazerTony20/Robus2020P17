a4b804cfac3c71112fffbd315b685f2000dd1154
Zacharie <zacharie.pare@usherbrooke.ca>
2020-11-11 21:50:30 -0500

    CODE A FUSIONNER

---
+++ Robus SN/code a fusionner/code.cpp
@@ -0,0 +1,261 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+#include <Fonctions.h>
+
+#define Servo1 1
+
+//void activerServo(uint8_t id_Servo,uint8_t angle_Servo);
+
+//0=moteur de gauche et 1=moteur de droite
+void setup()
+{
+  // put your setup code here, to run once:
+  BoardInit();
+}
+//----------//
+float Angle(float theta)
+{
+  // theta (entier): angle en degré à laquelle le robot doit virer
+
+  float nbLecture = 0;
+  nbLecture = (7979.002624 * (theta / 360));
+  //Serial.println(nbLecture);
+  return nbLecture;
+}
+//----------//
+// appeler virage pour tourner sur soi-même selon l'angle "angle". 0 = virage à gauche. 1 = virage à droite
+void virage(int sens, float angle)
+{
+  // sens (entier): 0 pour virer à gauche et 1 pour virer à droite
+  // angle (entier): angle du virage en degré
+  float lecture = (float)ENCODER_Read(0);
+  while ((float)ENCODER_Read(0) <= (lecture + Angle(angle)) && (float)ENCODER_Read(0) >= ((lecture - Angle(angle))))
+  {
+    // Serial.println(virageSelonAngle(angle));
+    if (sens == 1)
+    {
+      MOTOR_SetSpeed(0, 0.20);
+      MOTOR_SetSpeed(1, -0.20);
+    }
+    else if (sens == 0)
+    {
+      MOTOR_SetSpeed(0, -0.20);
+      MOTOR_SetSpeed(1, 0.20);
+    }
+  }
+  MOTOR_SetSpeed(0, 0);
+  MOTOR_SetSpeed(1, 0);
+  ENCODER_Reset(1);
+  ENCODER_Reset(0);
+  delay(100);
+}
+//----------//
+// appeler lignerDroite pour avancer selon la distance en mètre "distance".
+void tourner(int sens, float angle)
+{
+  float distance,distancelu = 0;
+  if (sens == 0)
+  {
+    distance = (19*3.1416*(angle/360))*300;
+   while(distance > distancelu)
+    {
+      delay(10);
+      MOTOR_SetSpeed(1,0.25);
+      distancelu = ENCODER_Read(1);
+    }
+  }
+   if (sens == 1)
+  {
+    distance = (19*3.1416*(angle/360))*300;
+   while(distance  > distancelu)
+    {
+      delay(10);
+      MOTOR_SetSpeed(0,0.25);
+      distancelu = ENCODER_Read(0);
+    }
+  }
+ 
+  MOTOR_SetSpeed(0, 0);
+  MOTOR_SetSpeed(1, 0);
+  delay(100);
+  ENCODER_Reset(0);
+  ENCODER_Reset(1);
+}
+//----------//
+void ligneDroite(float distance)
+{
+  //Pour aller en ligne droite sur une distance déterminée en mètre
+  float borne = 0;
+  borne = distance * 13367;
+  float kp = 0.0005;
+  float ki = 0.00003;
+  float errorp = 0;
+  float errori = 0;
+  
+  float speedright = 0.5;
+  float speedleft = 0.5;
+  float adjust = 0;
+  
+  float distancep = 0;
+  int nbcycle = 0;
+  MOTOR_SetSpeed(0, 0.15);
+  MOTOR_SetSpeed(1, 0.15);
+  delay(50);
+  MOTOR_SetSpeed(0, 0.25);
+  MOTOR_SetSpeed(1, 0.25);
+  delay(50);
+  MOTOR_SetSpeed(0, 0.35);
+  MOTOR_SetSpeed(1, 0.35);
+  delay(50);
+
+  MOTOR_SetSpeed(1, speedright);
+  MOTOR_SetSpeed(0, speedleft);
+  while (distancep < borne)
+  {
+    nbcycle++;
+    delay(200);
+    float valeur_voulue = ENCODER_ReadReset(0);
+    float valeur_reel = ENCODER_ReadReset(1);
+    distancep += valeur_reel;
+    errorp = valeur_voulue - valeur_reel;
+    errori = ((nbcycle * valeur_voulue) - distancep);
+    adjust = (errorp * kp) + (errori * ki);
+    MOTOR_SetSpeed(1, (speedright + adjust));
+  }
+  MOTOR_SetSpeed(0, 0.35);
+  MOTOR_SetSpeed(1, 0.35);
+  delay(50);
+  MOTOR_SetSpeed(0, 0.25);
+  MOTOR_SetSpeed(1, 0.25);
+  delay(50);
+  MOTOR_SetSpeed(0, 0.15);
+  MOTOR_SetSpeed(1, 0.15);
+  delay(50);
+  MOTOR_SetSpeed(0, 0);
+  MOTOR_SetSpeed(1, 0);
+  delay(100);
+  ENCODER_Reset(0);
+  ENCODER_Reset(1);
+}
+// appeler whelle pour instaurer la dominance.
+void whelle(int temps)
+{
+  //whellé d'un temps en millisecondes
+  MOTOR_SetSpeed(0, -1);
+  MOTOR_SetSpeed(1, -1);
+  delay(444);
+  MOTOR_SetSpeed(0, -0.75);
+  MOTOR_SetSpeed(1, -0.75);
+  delay(temps);
+  //virage(1, 135);
+  MOTOR_SetSpeed(0, 0);
+  MOTOR_SetSpeed(1, 0);
+}
+// appeler courbe pour tourner selon un arc de cercle. distance à parcourir en degré "angle" d'un cercle de rayon "rayon" en cm.
+// 0 = virage vers la gauche. 1 = virage vers la droite.
+void courbe(float angle, float rayon, int sens)
+{
+  float kp = 0.0001;
+  float ki = 0.00002;
+  float errorp;
+  float errori;
+  float speedright = 0.5;
+  float speedleft = 0.5;
+  float adjust;
+  float distance = 0;
+  int nbcycle = 0;
+  float distancegauche = 0;
+  float distancedroite = 0;
+  if (sens == 1)
+  {
+    distancegauche = (2 * 3.1416 * (rayon + 19) * (angle / 360));
+    distancedroite = (2 * 3.1416 * (rayon) * (angle / 360));
+  }
+  if (sens == 0)
+  {
+    distancegauche = (2 * 3.1416 * (rayon) * (angle / 360));
+    distancedroite = (2 * 3.1416 * (rayon + 19) * (angle / 360));
+  }
+  distance = distancedroite;
+  while (distance > distancedroite)
+  {
+    MOTOR_SetSpeed(1, (distancedroite / distancegauche) * speedright);
+    nbcycle++;
+    MOTOR_SetSpeed(0, speedleft);
+    delay(50);
+    float valeur_voulue = ((distancedroite / distancegauche) * ENCODER_Read(0));
+    float valeur_reel = ENCODER_Read(1);
+    distance += valeur_reel;
+    errorp = valeur_voulue - valeur_reel;
+    errori = ((nbcycle * valeur_voulue) - distance);
+    adjust = (errorp * kp) + (errori * ki);
+    MOTOR_SetSpeed(1, (speedright + adjust));
+    ENCODER_Reset(0);
+    ENCODER_Reset(1);
+  }
+  MOTOR_SetSpeed(0, 0.35);
+  MOTOR_SetSpeed(1, 0.35);
+  delay(50);
+  MOTOR_SetSpeed(0, 0.25);
+  MOTOR_SetSpeed(1, 0.25);
+  delay(50);
+  MOTOR_SetSpeed(0, 0.15);
+  MOTOR_SetSpeed(1, 0.15);
+  delay(50);
+  MOTOR_SetSpeed(0, 0);
+  MOTOR_SetSpeed(1, 0);
+  delay(50);
+}
+
+//----------//
+
+void loop()
+{
+  //VARIABLE
+   activerServo(Servo1,45);
+  // put your main code here, to run repeatedly:
+ if (sound_detection() == 0)
+ {
+  
+    
+    delay(200);
+    ligneDroite(0.31);
+    tourner(1,80);
+    ligneDroite(0.02);
+    tourner(0,82);
+    ligneDroite(0.235);
+    delay(500);
+    int color = couleur(3); //lire couleur
+    delay(500);
+    ligneDroite(1.16);
+    activerServo(Servo1,135);
+    delay(2500);
+
+    if(color == 2)
+    {
+    ligneDroite(0.81);
+    tourner(1,80);
+    ligneDroite(0.20);
+    delay(200000);
+    }
+
+   if(color == 3)
+   {
+     ligneDroite(1.66);
+    tourner(0,82);
+    ligneDroite(0.08);
+    delay(200000);
+   }
+
+
+   if(color == 1)
+   {
+    ligneDroite(2.29);
+    tourner(1,80);
+    ligneDroite(0.15);
+    delay(200000);
+   }
+    delay(200000);
+    
+  }
+}
---
+++ Robus SN/code a fusionner/galipette.h
@@ -0,0 +1,108 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+#include<Adafruit_TCS34725.h>
+
+//=======================================================DÉCLARATIONS========================================================//
+
+uint32_t soundLenght = 0;
+
+//=========================================================FONCTIONS=========================================================//
+
+//Fonction du ServoMoteur
+void activerServo(int id_Servo,int angle_Servo){
+    SERVO_Enable(id_Servo);
+    SERVO_SetAngle(id_Servo, angle_Servo);
+    delay(500);
+    SERVO_Disable(id_Servo);
+    delay(500);
+}
+
+//fonction couleur
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+int longif(int moyennea, int moyenneb, int moyennec);
+
+  int couleur(int nbfois)
+  {
+    uint16_t clear, red, green, blue;
+    int sommer = 0;
+    int sommeb = 0;
+    int sommeg = 0;
+    int moyenner = 0;
+    int moyenneg = 0;
+    int moyenneb = 0;
+      for(int i=0;i<nbfois;i++)
+      {
+        tcs.setInterrupt(false);
+        delay(60);
+        tcs.getRawData(&red, &green, &blue, &clear);
+        tcs.setInterrupt(true);
+        sommer+=red;
+        sommeg+=green;
+        sommeb+=blue;
+      }
+      moyenner=sommer/nbfois;
+      moyenneg=sommeg/nbfois;
+      moyenneb=sommeb/nbfois;
+      Serial.println(moyenner);
+      Serial.print("\n");
+      Serial.println(moyenneg);
+      Serial.print("\n");
+      Serial.println(moyenneb);
+      Serial.print("\n");
+      longif(moyenner, moyenneg, moyenneb);
+      return longif(moyenner, moyenneg, moyenneb);
+  }
+
+int longif(int moyennea, int moyenneb, int moyennec)
+  {
+    if(moyennea<=320 && moyennea>=300 && moyenneb<=290 && moyenneb>=275) //&& moyennec<=260 && moyennec>=245)
+    {
+      pinMode(11, OUTPUT);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, LOW);
+      digitalWrite(13, LOW);
+      return 1;
+    }
+    else if(moyennea<=335 && moyennea>=320 && moyenneb<=350 && moyenneb>=311) //&& moyennec<=290 && moyennec>=280)
+    {
+      pinMode(12, OUTPUT);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(13, LOW);
+      //analogWrite(A4, LOW);
+      return 2;
+    }
+    else if(moyennea<=299 && moyennea>=0 && moyenneb<=310 && moyenneb>=291) //&& moyennec<=255 && moyennec>=245)
+    {
+      pinMode(13, OUTPUT);
+      digitalWrite(13, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+      //analogWrite(A5, LOW);
+      return 3;
+    }
+    else 
+    {
+      return 0;
+    }
+  }
+
+  //Fonction de détection du son 5kHz
+int sound_detection()
+{
+  if(analogRead(A0) > 520)
+    {
+      soundLenght += 25;
+    }
+  else
+    {
+      soundLenght = 0;
+    }
+  delay(25);
+  if(soundLenght > 1200)
+    {
+      Serial.println("Son detecte");
+      return 0;
+    }
+  return 1;
+}
